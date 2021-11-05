d3717381c7920235145a7f82aa8c779fffee2c3b
Pablo Oliveira <pablo.almeida.ti@hotmail.com>
2021-11-05 15:10:37 -0300

    Organizing Structure in MVC Folders

---
+++ 07 Alura.ListaLeitura.Projeto.Inicial/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Alura.ListaLeitura.App.Logica;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/ParaLer", LivrosLogica.LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", LivrosLogica.LivrosLendo);
+            builder.MapRoute("Livros/Lidos", LivrosLogica.LivrosLidos);
+            builder.MapRoute("Livros/Detalhes/{id:int}", LivrosLogica.ExibeDetalhes);
+            builder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", CadastroLogica.NovoLivroParaLer);
+            builder.MapRoute("Cadastro/NovoLivro", CadastroLogica.ExibeFormulario);
+            builder.MapRoute("Cadastro/Incluir", CadastroLogica.ProcessaFormulario);
+
+            var rotas = builder.Build();
+
+            app.UseRouter(rotas);
+        }
+
+    }
+}
